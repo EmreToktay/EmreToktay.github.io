ec9a085e7f07c2aed046d32d1125889f33704527
EmreToktay <119887065+EmreToktay@users.noreply.github.com>
2023-09-21 15:04:15 +0300

    ne

---
+++ cv/README.md
@@ -0,0 +1,19 @@
+ 
+
+## About the Challenge:
+
+In the absence of data, I focused on a simplified approach. A couple of nuances to consider based on data constraints include:
+
+If each record signifies a unique purchase, then my calculation for the total purchase revenue should stand correct.
+Conversely, if the records indicate purchase types rather than individual transactions, a nuanced approach to calculation is warranted.
+## Tool Selection:
+
+I finished this challenge at Python for without platform change for visualization. Although I'm adept at using Google spreadsheet tools, 
+I gravitated towards Python due to my comfort level and expertise in it.
+
+# Regarding Power BI:
+
+I may not have deep expertise in Power BI, but my extensive background with Google Data Studio equips me well. 
+My holistic experience in data visualization, combined with a deep understanding of data integration and transformation, 
+and knowledge of DAX, assures me of my ability to adapt and excel in diverse platforms or tools as necessary.
+
---
+++ cv/case.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Convert the data to pandas DataFrames
+user_df = pd.DataFrame(user_data)
+purchase_df = pd.DataFrame(purchase_data)
+country_df = pd.DataFrame(country_data)
+media_source_df = pd.DataFrame(media_source_data)
+session_df = pd.DataFrame(session_data)
+
+# Calculations
+number_of_sessions = session_df['session_id'].nunique()
+count_users_with_sessions = session_df['user_id'].nunique()
+count_of_purchases = purchase_df['purchase_id'].nunique()
+total_purchase_revenue = purchase_df['purchase_revenue'].sum()
+revenue_per_purchase = total_purchase_revenue / count_of_purchases
+purchasing_users = purchase_df['user_id'].nunique()
+revenue_per_purchasing_user = total_purchase_revenue / purchasing_users
+sessions_with_purchases = purchase_df['purchase_session_id'].nunique()
+count_of_new_users = user_df['user_id'].nunique()
+
+
+# Bar plot for purchase revenue by country
+merged_df = pd.merge(purchase_df, user_df, on="user_id")
+merged_df = pd.merge(merged_df, country_df, left_on="user_country_id", right_on="Country_id")
+country_revenues = merged_df.groupby("country_name")["purchase_revenue"].sum()
+
+plt.figure(figsize=(12, 6))
+sns.barplot(x=country_revenues.index, y=country_revenues.values)
+plt.title("Total Purchase Revenue by Country")
+plt.ylabel("Total Revenue")
+plt.xlabel("Country")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# Histogram for distributoin of purchase revenues
+plt.figure(figsize=(12, 6))
+sns.histplot(purchase_df['purchase_revenue'], bins=50, kde=True)
+plt.title("Distribution of Purchase Revenues")
+plt.xlabel("Purchase Revenue")
+plt.ylabel("Count")
+plt.tight_layout()
+plt.show()
+
+# Line chart for number of purchases over time
+purchase_df['purchase_datetime'] = pd.to_datetime(purchase_df['purchase_datetime'])
+purchases_over_time = purchase_df.groupby(purchase_df['purchase_datetime'].dt.date).size()
+
+plt.figure(figsize=(12, 6))
+sns.lineplot(x=purchases_over_time.index, y=purchases_over_time.values)
+plt.title("Number of Purchases Over Time")
+plt.ylabel("Number of Purchases")
+plt.xlabel("Date")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
+# Scatterplot for session counts vs. purchase counts for each user
+session_counts = session_df.groupby("user_id").size()
+purchase_counts = purchase_df.groupby("user_id").size()
+
+# Merge both series into a DataFrame for visualization
+scatter_data = pd.merge(session_counts, purchase_counts, on="user_id", how="outer").fillna(0)
+scatter_data.columns = ['session_count', 'purchase_count']
+
+plt.figure(figsize=(12, 6))
+sns.scatterplot(data=scatter_data, x='session_count', y='purchase_count')
+plt.title("Session Counts vs. Purchase Counts for Users")
+plt.xlabel("Number of Sessions")
+plt.ylabel("Number of Purchases")
+plt.tight_layout()
+plt.show()
+
+# Box plot for distribution of purchase revenues by country
+plt.figure(figsize=(12, 6))
+sns.boxplot(data=merged_df, x="country_name", y="purchase_revenue")
+plt.title("Distribution of Purchase Revenues by Country")
+plt.xlabel("Country")
+plt.ylabel("Purchase Revenue")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
